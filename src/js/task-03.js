2250f6b03fc4668f8a9e4dc9aed2332ef01ab8d2
Rostyk Podolets <ros.podolets@gmail.com>
2021-09-15 10:04:22 +0300

    task-03
    Completed tasks: 03 of 08.

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,3 +1,4 @@
+// data
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +13,38 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// functions
+function makeGalleryItemMarkup({ url, alt }) {
+  return `
+    <li class="gallery__item">
+      <img class="gallery__picture" src="${url}" alt="${alt}">
+    </li>`;
+}
+
+function makeGalleryMarkup(array) {
+  return array.map(makeGalleryItemMarkup).join('');
+}
+
+// operations
+const result = makeGalleryMarkup(images);
+
+const galleryListRef = document.querySelector('#gallery');
+
+galleryListRef.insertAdjacentHTML('afterbegin', result);
+
+// style
+const styleRef = document.querySelector('style');
+const galleryStyle = `#gallery {
+  display: flex;
+  list-style: none;
+}
+.gallery__item {
+  flex-basis: (100 / 3) - 60px;
+  margin: 0 10px;
+}
+.gallery__picture {
+  height: 250px;
+}`;
+
+styleRef.insertAdjacentHTML('beforeend', galleryStyle);
